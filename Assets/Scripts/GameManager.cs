82a1abfe3c4f0090e03742c64a3e2a03cc8c1c7a
ArrmenLee <wachemanstonphoenix@gmail.com>
2022-01-05 06:18:48 +0800

    220105 Add pickup connection between player's behavior with GameManager/UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace COVID_RUSH
 {
     public class GameManager : MonoBehaviour
     {
+        private class Score
+        {
+            public int vaccineCount = 0;
+            public int facemaskCount = 0;
+            public int needleCount = 0;
+            public int time = 0;
+            public int VaccineScore { get { return 7 * vaccineCount; } }
+            public int NeedleScore { get { return 5 * vaccineCount; } }
+            public int FacemaskScore { get { return 3 * vaccineCount; } }
+            public int GetScore(int endTime)
+            {
+                return VaccineScore + NeedleScore + FacemaskScore - Mathf.Abs(endTime - time);
+            }
+
+            public void Reset()
+            {
+                vaccineCount = 0;
+                facemaskCount = 0;
+                needleCount = 0;
+                time = 0;
+            }
+        }
+
         public static GameManager instance = null;
         private int mCurrentLevel = 1;
         private int currentVolume = 50;
         private int mCurrentTiming = 0;
+        private int mCurrentNeedle = 0;
+        private int mCurrentFacemask = 0;
+        private Score mScore = new Score();
 
         private EventStore EventManager = EventStore.instance;
         public enum GameState : int { Start, Information, Setting, Gaming, Wasted, LevelEnd, Ended }
+
+        private class ItemType
+        {
+            public const string Vaccine = "Props_Vaccine";
+            public const string Facemask = "Props_Facemask";
+            public const string Needle = "Props_Needle";
+
+            public bool Contain(string key)
+            {
+                return (Vaccine == key) || (Facemask == key) || (Needle == key);
+            }
+        }
+        private ItemType mItemType = new ItemType();
 
         private GameState mGameState = GameState.Start;
 
@@ -40,6 +80,7 @@ namespace COVID_RUSH
         {
             EventManager.Register("showWasted", this, (c,p) => ShowWasted());
             EventManager.Register("onSetLevelTiming", this, (_, p) => SetTiming(p));
+            EventManager.Register("onPickupItem", this, (_, p) => HandlePickUp(p));
         }
 
         private void FixedUpdate()
@@ -90,7 +131,7 @@ namespace COVID_RUSH
                 yield return new WaitForSeconds(2);
                 // TODO: Use an enum to map state-to-code
                 // 1 = scene of level 1
-                EventManager.Notify("onLoadScene", this, mCurrentLevel);
+                EventManager.Notify("onLoadScene", this, 2);
                 yield return new WaitForSeconds(1);
                 StartCountdown();
                 yield return new WaitForSeconds(4);
@@ -137,6 +178,21 @@ namespace COVID_RUSH
             {
                 SetLifeValue();
             }
+            
+            if (Input.GetKeyDown(KeyCode.F6))
+            {
+                SetNeedleCountByDiff(1);
+                SetFacemaskCountByDiff(1);
+            }
+            if (Input.GetKeyDown(KeyCode.F7))
+            {
+                SetNeedleCountByDiff(-1);
+                SetFacemaskCountByDiff(-1);
+            }
+            if (Input.GetKeyDown(KeyCode.F8))
+            {
+                PickUpVaccine();
+            }
         }
 
         private void ShowWasted()
@@ -171,6 +227,50 @@ namespace COVID_RUSH
         private void SetLifeValue ()
         {
             EventManager.Notify("onSetLifeValueByDiff", this, -8);
+        }
+
+        private void PickUpVaccine()
+        {
+            mScore.vaccineCount++;
+            EventManager.Notify("onSetLifeValueByDiff", this, 20);
+        }
+
+        private void SetNeedleCountByDiff(int diff)
+        {
+            mCurrentNeedle = Mathf.Clamp(mCurrentNeedle + diff, 0, 5);
+            mScore.needleCount = mCurrentFacemask;
+            string value = String.Concat(Enumerable.Repeat("y", mCurrentNeedle));
+            Dictionary<string, string> dict = new Dictionary<string, string>
+            {
+                { "needleCount", value },
+            };
+            EventManager.Notify("onVariableChange", this, dict);
+        }
+        private void SetFacemaskCountByDiff(int diff)
+        {
+            mCurrentFacemask = Mathf.Clamp(mCurrentFacemask + diff, 0, 5);
+            mScore.facemaskCount = mCurrentFacemask;
+            string value = String.Concat(Enumerable.Repeat("m", mCurrentFacemask));
+            Dictionary<string, string> dict = new Dictionary<string, string>
+            {
+                { "facemaskCount", value },
+            };
+            EventManager.Notify("onVariableChange", this, dict);
+        }
+
+        private void HandlePickUp(object tgN)
+        {
+            string tagName = (string)tgN;
+            if (mItemType.Contain(tagName))
+            {
+                switch (tagName)
+                {
+                    case ItemType.Vaccine: PickUpVaccine(); break;
+                    case ItemType.Needle: SetNeedleCountByDiff(1);  break;
+                    case ItemType.Facemask: SetFacemaskCountByDiff(1); break;
+                    default: break;
+                }
+            }
         }
     }
 }
